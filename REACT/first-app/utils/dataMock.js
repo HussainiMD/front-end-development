b67207f6b89e807bc732b6f22533e2ce00aba3c2
HussainiMD <43377755+HussainiMD@users.noreply.github.com>
2024-12-31 12:24:41 +0530

    Create dataMock.js

---
+++ REACT/first-app/utils/dataMock.js
@@ -0,0 +1,102 @@
+export default restaurantDetailsList = [
+    {
+        id: 4321,
+        logo: 'https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/RX_THUMBNAIL/IMAGES/VENDOR/2024/7/17/7b659f73-404c-446e-9faf-8b039f5ac10f_340856.jpg',            
+        cusines: ['Pizzas'],
+        dish: 'Pizza Hut',
+        rating: 3.2,
+        location: 'Tolichowki',
+        estimatedTime: '30-45 mins',
+        offer: '50% OFF UPTO 100'          
+    },
+    {
+        id: 4322,
+        logo: 'https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/RX_THUMBNAIL/IMAGES/VENDOR/2024/11/29/fd725143-e02f-4eec-8d91-7fd49d7c8303_24635.jpg',            
+        cusines: ['Pizzas', 'Italian', 'Pastas', 'Desserts'],
+        dish: 'Pizza Domino\'s',
+        rating: 4.3,
+        location: 'Film Nagar',
+        estimatedTime: '20-25 mins',
+        offer: '$125 OFF ABOVE $1199'          
+    },
+    {
+        id: 4323,
+        logo: 'https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/RX_THUMBNAIL/IMAGES/VENDOR/2024/5/9/39a65ace-1068-4823-86bf-887991751e2a_362655.JPG',            
+        cusines: ['Burgers', 'Fast Food', 'Rolls & Wraps'],
+        dish: 'KFC',
+        rating: 2.1,
+        location: 'Manikonda',
+        estimatedTime: '30-35 mins',
+        offer: 'ITEMS AT $99'          
+    },
+    {
+        id: 4324,
+        logo: 'https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/c583ca6ce40b426797a78ae2ac91f2ec',            
+        cusines: ['Burgers', 'Fast Food', 'Rolls & Wraps'],
+        dish: 'LeanCrust Pizza- ThinCrust Experts',
+        rating: 4.1,
+        location: 'Manikonda',
+        estimatedTime: '30-35 mins',
+        offer: '60% OFF UPTO $99'          
+    },
+    {
+        id: 4325,
+        logo: 'https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/RX_THUMBNAIL/IMAGES/VENDOR/2024/12/9/e04b665e-f3dd-4bc5-be0a-c74f44645328_690583.jpg',            
+        cusines: ['Burgers', 'Fast Food', 'Rolls & Wraps'],
+        dish: 'Daily Kitchen - Homely Meals',
+        rating: 3.1,
+        location: 'Manikonda',
+        estimatedTime: '30-35 mins',
+        offer: '$99 OFF ON $999 BILL'          
+    },
+    {
+        id: 4326,
+        logo: 'https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/kwvmoc0nvrzuh5nshk7z',            
+        cusines: ['Pizzas', 'Italian', 'Pastas', 'Desserts'],
+        dish: 'Mandi @36',
+        rating: 4.3,
+        location: 'Film Nagar',
+        estimatedTime: '20-25 mins',
+        offer: '$125 OFF ABOVE $1199'          
+    },
+    {
+        id: 4327,
+        logo: 'https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/RX_THUMBNAIL/IMAGES/VENDOR/2024/12/9/c6d40090-830e-4112-9c02-d86ca776ca5f_7235.JPG',            
+        cusines: ['Pizzas', 'Italian', 'Pastas', 'Desserts'],
+        dish: 'WeFit - Protein Bowls, Salads & Sandwiches',
+        rating: 4.3,
+        location: 'Film Nagar',
+        estimatedTime: '20-25 mins',
+        offer: '$125 OFF ABOVE $699'          
+    },
+    {
+        id: 4328,
+        logo: 'https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/RX_THUMBNAIL/IMAGES/VENDOR/2024/4/17/f92c604d-68cb-49db-8dec-9473650c363d_728895.JPG',            
+        cusines: ['Burgers', 'Fast Food', 'Rolls & Wraps'],
+        dish: 'Cafe Niloufer Premium Lounge',
+        rating: 2.1,
+        location: 'Manikonda',
+        estimatedTime: '30-35 mins',
+        offer: 'FLAT OFF 10%'          
+    },
+    {
+        id: 4329,
+        logo: 'https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/RX_THUMBNAIL/IMAGES/VENDOR/2024/5/9/39a65ace-1068-4823-86bf-887991751e2a_362655.JPG',            
+        cusines: ['Burgers', 'Fast Food', 'Rolls & Wraps'],
+        dish: 'KFC',
+        rating: 2.1,
+        location: 'Manikonda',
+        estimatedTime: '30-35 mins',
+        offer: 'ITEMS AT $99'          
+    },
+    {
+        id: 4330,
+        logo: 'https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/c583ca6ce40b426797a78ae2ac91f2ec',            
+        cusines: ['Burgers', 'Fast Food', 'Rolls & Wraps'],
+        dish: 'LeanCrust Pizza- ThinCrust Experts',
+        rating: 4.1,
+        location: 'Manikonda',
+        estimatedTime: '30-35 mins',
+        offer: '60% OFF UPTO $99'          
+    }
+]
